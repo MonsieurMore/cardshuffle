f53992eec037c31d1d4e62b3b1afa69adbc92b8b
Monsieur More <monsieur.more@gmail.com>
2023-12-25 18:43:40 +0100

    Premier essai - structure globale

---
+++ strucure.py
@@ -0,0 +1,48 @@
+
+# Script de création de bibliothèque de JCC en revente
+# Le script permet de créer une base de données intégrant des dictionnaires de cartes, de propriétaires, de prix de vente,
+# et de taux de commission de revente afin de calculer les bénéfices réalisés et répartir les gains
+
+# Menu de lancement -Structure conditionnelle à choix multiples
+
+import numpy as np
+import pandas as pd
+import json as js # création du dictionnaire de cartes
+
+
+def add_card(ajouter):
+    ajouter =
+
+def remove_card():
+
+def add_seller():
+
+def check_seller():
+
+
+choix = int(input("Entrez votre choix entre 1 et 4"))
+while True
+if choix = [1..4] 
+    match (choix)
+
+#ajouter une carte
+        case 1:
+            print("Ajouter une carte")
+            add_card()
+
+#retirer une carte
+        case 2:
+            print("Retirer une carte")
+            remove_card()
+
+#ajouter un vendeur
+        case 3:
+            print("Ajouter un vendeur")
+            add_seller()
+
+#consulter un vendeur
+        case 4:
+            print("Consulter un vendeur")
+            check_seller()
+else:
+    break        
